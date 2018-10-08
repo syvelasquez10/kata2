9a25afce84097b045ecfed910c19cd7931426ff3
Sergio Velasquez <sy.velasquez10@uniandes.edu.co>
2018-10-07 22:33:10 -0500

    Rojo: Test fallando probar el registro de un independiente

--- polls/Functional_test.py
+++ polls/Functional_test.py
@@ -18,3 +18,37 @@ class FunctionalTest(TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('Busco Ayuda', self.browser.title)
 
+    def test_registro(self):
+        self.browser.get('http://localhost:8000')
+        link = self.browser.find_element_by_id('id_register')
+        link.click()
+
+        nombre = self.browser.find_element_by_id('id_nombre')
+        nombre.send_keys('Juan Daniel')
+
+        apellidos = self.browser.find_element_by_id('id_apellidos')
+        apellidos.send_keys('Arevalo')
+
+        experiencia = self.browser.find_element_by_id('id_aniosExperiencia')
+        experiencia.send_keys('5')
+
+        self.browser.find_element_by_xpath("//select[@id='id_tiposDeServicio']/option[text()='TiposDeServicio object (1)']").click()
+        telefono = self.browser.find_element_by_id('id_telefono')
+        telefono.send_keys('3173024578')
+
+        correo = self.browser.find_element_by_id('id_correo')
+        correo.send_keys('jd.patino1@uniandes.edu.co')
+
+
+        nombreUsuario = self.browser.find_element_by_id('id_username')
+        nombreUsuario.send_keys('juan645')
+
+        clave = self.browser.find_element_by_id('id_password')
+        clave.send_keys('clave123')
+
+        botonGrabar = self.browser.find_element_by_id('id_grabar')
+        botonGrabar.click()
+        self.browser.implicitly_wait(3)
+        span=self.browser.find_element(By.XPATH, '//span[text()="Juan Daniel Arevalo"]')
+
+        self.assertIn('Juan Daniel Arevalo', span.text)
